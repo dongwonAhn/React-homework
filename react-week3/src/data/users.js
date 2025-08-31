d93fb16448144d05e6a8c0b3070ffe78207981c8
dongwonAhn <ado2188@naver.com>
2025-09-01 00:19:14 +0900

    feat: 유저 데이터 추가

---
+++ react-week3/src/data/users.js
@@ -0,0 +1,38 @@
+export const USERS = [
+  {
+    id: 1,
+    name: "Lelah Nichols",
+    role: "Frontend Engineer",
+    avatar: "/users/user-01.svg",
+  },
+  {
+    id: 2,
+    name: "Jesus Weiss",
+    role: "UI/UX Designer",
+    avatar: "/users/user-02.svg",
+  },
+  {
+    id: 3,
+    name: "Annie Rice",
+    role: "Product Manager",
+    avatar: "/users/user-03.svg",
+  },
+  {
+    id: 4,
+    name: "Robert Brower",
+    role: "Full-stack Dev",
+    avatar: "/users/user-04.svg",
+  },
+  {
+    id: 5,
+    name: "Amy Campbell",
+    role: "QA Engineer",
+    avatar: "/users/user-05.svg",
+  },
+  {
+    id: 6,
+    name: "Anthony S. Morin",
+    role: "CEO",
+    avatar: "/users/user-06.svg",
+  },
+];
